a7629c581747e2bad0e451ef38bf99b9a6140bf3
Tim Monroe <timonroe12@gmail.com>
2024-01-04 11:30:35 -0500

    added the class declarations back into the index.d.ts file so clients have access to them

--- news/index.d.ts
+++ news/index.d.ts
@@ -25,3 +25,33 @@ export declare type NewsScraperResponse = {
 export interface NewsScraper {
   scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
 }
+
+export declare class APScraper implements NewsScraper {
+  constructor();
+  scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
+}
+
+export declare class CNNScraper implements NewsScraper {
+  constructor();
+  scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
+}
+
+export declare class EpochTimesScraper implements NewsScraper {
+  constructor();
+  scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
+}
+
+export declare class FoxScraper implements NewsScraper {
+  constructor();
+  scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
+}
+
+export declare class WashExamScraper implements NewsScraper {
+  constructor();
+  scrape(type: NewsScraperType): Promise<NewsScraperResponse>;
+}
+
+export declare class NewsScraperFactor {
+  constructor();
+  createScrapers(sources: NewsScraperSource[]): Promise<NewsScraper[]>;
+}
